045f4e61b29f033a49b2e42bbc41e75b13909a88
imneetkaur <imneetkaur007@gmail.com>
2019-05-15 11:08:16 +0530

    Task5 Done

--- src/main/java/com.stackroute/domain/Movie.java
+++ src/main/java/com.stackroute/domain/Movie.java
@@ -1,20 +1,30 @@
 package com.stackroute.domain;
 
-public class Movie {
+import com.stackroute.domain.Actor;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie  implements ApplicationContextAware, BeanNameAware , BeanFactoryAware {
     private String movieName;
     private String genre;
     Actor actor;
+    private ApplicationContext applicationContext =null;
 
     public Movie() {
-        System.out.println("MOVIE");
+        //System.out.println("MOVIE");
     }
-    //constructor injection
+    //constructor
     public Movie(Actor actor){
         this.actor=actor;
     }
     public Movie(String movieName, String genre,Actor actor) {
         this.movieName = movieName;
         this.genre = genre;
+        this.actor=actor;
     }
 
     public String getMovieName() {
@@ -24,7 +34,7 @@ public class Movie {
     public void setMovieName(String movieName) {
         this.movieName = movieName;
     }
-    //SetterInjection
+    //Setters
     public void setActor(Actor actor){
         this.actor=actor;
     }
@@ -35,7 +45,20 @@ public class Movie {
     public void setGenre(String genre) {
         this.genre = genre;
     }
-
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException
+    {
+        this.applicationContext = context;
+    }
+    @Override
+    public void setBeanName(String beanName)
+    {
+        System.out.println("Bean name is: "+beanName);
+    }
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean Factory is  " +beanFactory);
+    }
 
 
     @Override
@@ -46,4 +69,6 @@ public class Movie {
                 ", actor=" + actor +
                 '}';
     }
+
+
 }
